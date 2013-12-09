8f64a68e4efdbddec16abd0b6e6f0509596422f7
mihir <mihir.khatwani@gmail.com>
2013-12-09 17:18:49 +0530

    Removed AWS

--- pari/settings/heroku.py
+++ pari/settings/heroku.py
@@ -15,19 +15,19 @@ INSTALLED_APPS += (
     # "storages",
 )
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_QUERYSTRING_AUTH = False
+#AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'pari.article.storage.ParallelS3Storage'
 # STATICFILES_STORAGE = 'pari.article.storage.StaticRootS3BotoStorage'
 # COMPRESS_STORAGE = DEFAULT_FILE_STORAGE
 
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 # STATIC_URL = S3_URL
 # COMPRESS_URL = STATIC_URL
-MEDIA_URL = S3_URL + "media/"
+#MEDIA_URL = S3_URL + "media/"
 # MEDIA_ROOT = ''
 
 # STATICFILES_FINDERS += (
